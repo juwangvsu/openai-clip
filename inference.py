046769d151d242e2f9ba0d0dc3a7c6c666fe8824
Ju Wang <jwang@vsu.edu>
2024-05-30 09:25:00 -0400

    update

--- inference.py
+++ inference.py
@@ -21,6 +21,7 @@ def get_image_embeddings(valid_df, model_path):
     valid_image_embeddings = []
     with torch.no_grad():
         for batch in tqdm(valid_loader):
+            print('batch keys, bsize', len(batch), batch.keys(), len(batch['caption']))
             image_features = model.image_encoder(batch["image"].to(CFG.device))
             image_embeddings = model.image_projection(image_features)
             valid_image_embeddings.append(image_embeddings)
@@ -29,22 +30,29 @@ def get_image_embeddings(valid_df, model_path):
 def find_matches(model, image_embeddings, query, image_filenames, n=9):
     tokenizer = DistilBertTokenizer.from_pretrained(CFG.text_tokenizer)
     encoded_query = tokenizer([query])
+    print('encoded query  ', encoded_query)
     batch = {
         key: torch.tensor(values).to(CFG.device)
         for key, values in encoded_query.items()
     }
+    print('query sent to gpu', batch)
     with torch.no_grad():
         text_features = model.text_encoder(
             input_ids=batch["input_ids"], attention_mask=batch["attention_mask"]
         )
+        print('text_features ', text_features.shape)
         text_embeddings = model.text_projection(text_features)
+        print('text_embeddings ', text_embeddings.shape)
     
     image_embeddings_n = F.normalize(image_embeddings, p=2, dim=-1)
     text_embeddings_n = F.normalize(text_embeddings, p=2, dim=-1)
     dot_similarity = text_embeddings_n @ image_embeddings_n.T
+    print('image_embeddings ', image_embeddings_n.shape)
+    print('similarity mat ', dot_similarity.shape)
     
-    _, indices = torch.topk(dot_similarity.squeeze(0), n * 5)
-    matches = [image_filenames[idx] for idx in indices[::5]]
+    topsimilar, indices = torch.topk(dot_similarity.squeeze(0), n * 5)
+    print('top similarity ', indices, dot_similarity.squeeze(0)[1], topsimilar)
+    matches = [image_filenames[idx] for idx in indices[::5]]#each image has 5 text desc in this dataset, so skip every 5 image
     
     _, axes = plt.subplots(3, 3, figsize=(10, 10))
     for match, ax in zip(matches, axes.flatten()):
